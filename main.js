c6ad23e14ce42861b6b518c8722355ecdbe2bcb6
hyukjekwon <hyukjekwon@gmail.com>
2023-07-22 20:30:43 -0400

    Made projects tab a dropdown, three projects are now accessible.

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 "use strict";
 
 const $ = (id) => document.getElementById(id);
+const three_projects = ['joy-division/joydivision.html', 'particle-movement/movement.html',
+                        'pendulum-simulation/pendulum.html', 'tree-generator/tree.html']
 
-function loadPage(pageId) {
-    // Initialize home page
+function loadPage(page) {
     const main = $("main");
-    main.data = `subpages/${pageId}.html`
-    // main.innerHTML = `<object type="text/html" data="subpages/${pageId}.html" id="main"></object>`
+    main.data = page;
+}
+
+function loadMainPage(pageId) {
+    // Initialize home page
+    loadPage(`subpages/${pageId}.html`);
 }
 
 function initAll() {
-    const navLinks = document.getElementsByClassName("nav-link");
+    const navLinks = document.getElementsByClassName("main-subpage");
     for (const link of navLinks) {
-        link.addEventListener("click", e => {
-            loadPage(link.id.split("-")[0]);
+        link.addEventListener("click", () => {
+            loadMainPage(link.id.split("-")[0]);
         })
+    }
+    const projectDropDown = $('project-dropdown');
+    for (const project of three_projects) {
+        const projName = project.split('/')[0];
+        const projTag = document.createElement('a');
+        projTag.classList.add("dropdown-item");
+        projTag.href = "#";
+        projTag.id = projName;
+        projTag.innerText = projName;
+        projTag.addEventListener("click", () => {
+            loadPage(project);
+        });
+        projectDropDown.appendChild(projTag);
     }
 }
 
